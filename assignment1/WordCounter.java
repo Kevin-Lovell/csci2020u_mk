dfb8ead5572d0dd92dbc143df525b11108882dc0
aadnan <aadnan_z@hotmail.com>
2016-03-08 16:04:47 -0500

    Fixed: Removes duplicates from email before counting; Bug: Cannot iterate through string

--- assignment1/WordCounter.java
+++ assignment1/WordCounter.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class WordCounter {
     private Map<String,Integer> trainHamFreq;
 	private Map<String,Integer> trainSpamFreq;
-    
+
     public WordCounter() {
         trainHamFreq = new TreeMap<>();
+		
     }
     
     public void processFile(File file) throws IOException {
@@ -15,23 +16,59 @@ public class WordCounter {
             File[] filesInDir = file.listFiles();
             for (int i = 0; i < filesInDir.length; i++) {
                 processFile(filesInDir[i]);
+break;
             }
         } else if (file.exists()) {
-            // load all of the data, and process it into words
+            // load all of the data, deletes duplicates and process it into words
             Scanner scanner = new Scanner(file);
+			
+            String email = "";
             while (scanner.hasNext()) {
-                String word = scanner.next();
-                if (isWord(word)) {
-                    trainHamFreq(word);
+				String word = scanner.next();
+                if ((isWord(word))) {
+                    email = email + " " + word;
+					//trainHamFreq(word);
                 }
+				 email = removeDuplicates(email, "\\ ");
+            }
+		    System.out.println(email);
+           /*
+			String sArray[] = email.split(" ");
+			int j = sArray.length-1;
+            do {
+                String word = sArray[j];
+                System.out.println(word);
+				if ((isWord(word))) {
+					trainHamFreq(word);
+                }
+		        j--;
+            } while(j != -1);
+             */   
+        }
+    }
+
+    public static String removeDuplicates(String email, String splitRe)
+    {
+        List<String> values = new ArrayList<String>();
+        String[] splitted = email.split(splitRe);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < splitted.length; ++i)
+        {
+            if (!values.contains(splitted[i]))
+            {
+                values.add(splitted[i]);
+                sb.append(' ');
+                sb.append(splitted[i]);
             }
         }
+        return sb.substring(1);
+
     }
     
     private void trainHamFreq(String word) {
         if (trainHamFreq.containsKey(word)) {
-            int oldCount = trainHamFreq.get(word);
-            trainHamFreq.put(word, oldCount + 1);
+			int oldCount = trainHamFreq.get(word);
+            trainHamFreq.put(word, oldCount+1);
         } else {
             trainHamFreq.put(word, 1);
         }
@@ -67,8 +104,8 @@ public class WordCounter {
                 int count = trainHamFreq.get(key);
                 
                 if (count >= minCount) {
-                    //System.out.println(key + ": " + count);
-                    fout.println(key + ": " + count);
+                 //   System.out.println(key + ": " + count);
+					fout.println(key + ": " + count);
                 }
             }
             fout.close();
