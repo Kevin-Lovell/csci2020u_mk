acdcccc51f5df99d14b67434eda78d730fe6c5ce
aadnan <aadnan_z@hotmail.com>
2016-03-08 19:47:17 -0500

    Fixed all bugs; Program now counts occurance of a word per file

--- assignment1/WordCounter.java
+++ assignment1/WordCounter.java
@@ -7,65 +7,84 @@ public class WordCounter {
 
     public WordCounter() {
         trainHamFreq = new TreeMap<>();
-		
+	    trainSpamFreq = new TreeMap<>();
     }
     
-    public void processFile(File file) throws IOException {
+    public void processFile(File file, String folderFrom) throws IOException {
         if (file.isDirectory()) {
-            // process all of the files recursively
+            //process all of the files recursively
             File[] filesInDir = file.listFiles();
             for (int i = 0; i < filesInDir.length; i++) {
-                processFile(filesInDir[i]);
-break;
+                processFile(filesInDir[i], folderFrom);
             }
         } else if (file.exists()) {
-            // load all of the data, deletes duplicates and process it into words
+            //load all of the data and process it into words
             Scanner scanner = new Scanner(file);
-			
             String email = "";
             while (scanner.hasNext()) {
 				String word = scanner.next();
                 if ((isWord(word))) {
                     email = email + " " + word;
-					//trainHamFreq(word);
                 }
-				 email = removeDuplicates(email, "\\ ");
             }
-		    System.out.println(email);
-           /*
-			String sArray[] = email.split(" ");
-			int j = sArray.length-1;
-            do {
-                String word = sArray[j];
-                System.out.println(word);
-				if ((isWord(word))) {
-					trainHamFreq(word);
-                }
-		        j--;
-            } while(j != -1);
-             */   
+            //deletes duplicates from scanner(twice to avoid duplicates)
+            email = removeDupes(email, "\\ ");
+            email = removeDupes(email, "\\ ");
+
+            //puts the string back into a temporary textfile to make it easier to reuse some previous code
+			try{
+				//creates a temp file
+				File tmp = new File("temp.txt"); 
+				
+                //deletes file on exit
+				tmp.deleteOnExit();
+				
+                if (tmp.createNewFile()){
+                    //writes to temp file for the first time
+				    BufferedWriter bw = new BufferedWriter(new FileWriter(tmp));
+				    bw.write(email);
+        			bw.close();
+                } else {
+	                //rewrites to temp file if it already exists
+				    BufferedWriter bw = new BufferedWriter(new FileWriter(tmp));
+				    bw.write(email);
+        			bw.close();
+                }				
+                
+                //processes the tmp file to scanner, then puts it into treemap
+				Scanner scan = new Scanner(tmp);
+				while (scan.hasNext()) {
+					String word = scan.next();
+                    if(folderFrom == "ham") {
+					    countHam(word);	
+                    } else if (folderFrom == "spam") {
+                        countSpam(word);
+                    }				 
+				}				
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
         }
     }
 
-    public static String removeDuplicates(String email, String splitRe)
-    {
+    public static String removeDupes(String email, String splitRe) {
         List<String> values = new ArrayList<String>();
+        //splits the string version of the email into single words
         String[] splitted = email.split(splitRe);
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < splitted.length; ++i)
-        {
-            if (!values.contains(splitted[i]))
-            {
+        //if the list doesn't contain the word, add it to the string, if it does, then don't add to list
+        for (int i = 0; i < splitted.length; ++i) {
+            if (!values.contains(splitted[i])) {
                 values.add(splitted[i]);
                 sb.append(' ');
                 sb.append(splitted[i]);
             }
         }
+        //returns duplicate free version of string
         return sb.substring(1);
-
     }
     
-    private void trainHamFreq(String word) {
+    private void countHam(String word) {
         if (trainHamFreq.containsKey(word)) {
 			int oldCount = trainHamFreq.get(word);
             trainHamFreq.put(word, oldCount+1);
@@ -74,14 +93,14 @@ break;
         }
     }
     
-	 /*    private void countSpam(String word) {
-        if (wordCounts.containsKey(word)) {
-            int oldCount = wordCounts.get(word);
-            wordCounts.put(word, oldCount + 1);
+   private void countSpam(String word) {
+        if (trainSpamFreq.containsKey(word)) {
+            int oldCount = trainSpamFreq.get(word);
+            trainSpamFreq.put(word, oldCount + 1);
         } else {
-            wordCounts.put(word, 1);
+            trainSpamFreq.put(word, 1);
         }
-    } */
+    }
 	
     private boolean isWord(String str){
         String pattern = "^[a-zA-Z]*$";
@@ -91,22 +110,36 @@ break;
         return false;   
     }
     
-    public void printWordCounts(int minCount, File outputFile) throws FileNotFoundException {
+    public void printWordCounts(int minCount, File outputFile, String folderFrom) throws FileNotFoundException {
         System.out.println("Saving word counts to " + outputFile.getAbsolutePath());
         if (!outputFile.exists() || outputFile.canWrite()) {
             PrintWriter fout = new PrintWriter(outputFile);
-                        
-            Set<String> keys = trainHamFreq.keySet();
-            Iterator<String> keyIterator = keys.iterator();
-            
-            while(keyIterator.hasNext()) {
-                String key = keyIterator.next();
-                int count = trainHamFreq.get(key);
+            if(folderFrom == "ham") {
+                Set<String> keys = trainHamFreq.keySet();
+                Iterator<String> keyIterator = keys.iterator();
                 
-                if (count >= minCount) {
-                 //   System.out.println(key + ": " + count);
-					fout.println(key + ": " + count);
-                }
+                while(keyIterator.hasNext()) {
+                    String key = keyIterator.next();
+                    int count = trainHamFreq.get(key);
+                    
+                    if (count >= minCount) {
+                        //System.out.println(key + ": " + count);
+				        fout.println(key + ": " + count);
+                    }
+                } 
+            } else if(folderFrom == "spam") {
+                Set<String> keys = trainSpamFreq.keySet();
+                Iterator<String> keyIterator = keys.iterator();
+                
+                while(keyIterator.hasNext()) {
+                    String key = keyIterator.next();
+                    int count = trainSpamFreq.get(key);
+                    
+                    if (count >= minCount) {
+                        //System.out.println(key + ": " + count);
+				        fout.println(key + ": " + count);
+                    }
+                } 
             }
             fout.close();
         } else {
@@ -125,8 +158,18 @@ break;
         System.out.println("File: " + dataDir);
 
         try {
-            wordCounter.processFile(dataDir);
-            wordCounter.printWordCounts(2, new File(args[1]));
+            //checks what folder is the program looking at right now
+            if(dataDir.getName().contains("ham")) {
+                wordCounter.processFile(dataDir,"ham");
+                //first parameter is for minimum # of appearances the word needs to be shown on list(ie: 2 = print all
+                //values that are appear 2x, ignore all words that appear once)
+                wordCounter.printWordCounts(2, new File(args[1]),"ham");
+            } else if (dataDir.getName().contains("spam")) {
+                wordCounter.processFile(dataDir,"spam");
+                //first parameter is for minimum # of appearances the word needs to be shown on list(ie: 2 = print all
+                //values that are appear 2x, ignore all words that appear once)
+                wordCounter.printWordCounts(2, new File(args[1]),"spam");
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
